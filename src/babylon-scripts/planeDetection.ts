02f15b7e4b33c91c3df9ed78fc2d283cf05ca4ab
juan <juanlopezpem@gmail.com>
2024-03-11 16:21:57 +0100

    Posiconamiento del modelo con los planos

--- src/babylon-scripts/planeDetection.ts
+++ src/babylon-scripts/planeDetection.ts
@@ -23,6 +23,10 @@ type classArguments = {
     xr: BABYLON.WebXRDefaultExperience
 }
 
+interface IWebXRPlaneWithMesh extends IWebXRPlane {
+    mesh?: Mesh;
+}
+
 export class XrExperienceWithPlaneDetection {
     _scene: Scene;
     _xr: WebXRDefaultExperience;
@@ -54,15 +58,12 @@ export class XrExperienceWithPlaneDetection {
             console.log("Detección de planos activa");
             
         } catch (error) {
-            console.log(error);
+            console.error(error);
         }
     }
 
     createPlaneMeshesFromXrPlane(): void {
-        interface IWebXRPlaneWithMesh extends IWebXRPlane {
-            mesh?: Mesh;
-        }
-
+       
         let mat: Nullable<StandardMaterial>;
 
         if (this._xrPlanes === null) {
@@ -70,12 +71,71 @@ export class XrExperienceWithPlaneDetection {
         }
 
         let model = createModel(this._scene);
+        model.scaling._x = model.scaling._x * 2;
+
+        let normal = new BABYLON.Vector3();
+        let position = new BABYLON.Vector3();
+        let floorNormal = new BABYLON.Vector3();
+        let wallNormal = new BABYLON.Vector3();
+        let conH = 0;
+        let conV = 0;
+        let cubeSize = 0.1;
 
         this._xrPlanes.onPlaneAddedObservable.add((plane: IWebXRPlaneWithMesh) => {
             mat = new StandardMaterial("mat", this._scene);
             mat.alpha = 0.35;
             mat.diffuseColor = Color3.Random();
             this.initPolygon(plane, mat);
+            
+            let transformationMatrix = plane.transformationMatrix.m;
+            let planeMatrix = BABYLON.Matrix.FromArray(transformationMatrix);
+
+            console.log("PlaneMatrix:");
+            console.log(planeMatrix);
+
+            normal = new BABYLON.Vector3(
+                planeMatrix.m[8],
+                planeMatrix.m[9],
+                planeMatrix.m[10]
+            );
+
+            normal.normalize();
+
+            position = BABYLON.Vector3.TransformCoordinates(
+                BABYLON.Vector3.Zero(),
+                planeMatrix
+            );
+
+            if(plane.xrPlane.orientation.match("Horizontal")){
+                console.log("Horizontal plane");
+
+                if (conH < 1) {
+                    floorNormal = normal;
+                    console.log(floorNormal);
+                    model.position.y = position.y + cubeSize / 2;
+      
+                    conH++;
+                }
+                
+            }else if(plane.xrPlane.orientation.match("Vertical")){
+                console.log("Vertical plane");
+
+                if (conV < 1) {
+                    console.log(wallNormal);
+                    model.position.x = position.x - cubeSize / 2;
+                    model.position.z = position.z - cubeSize / 2;
+      
+                    const planeMatrix = BABYLON.Matrix.FromArray(transformationMatrix);
+                    const planeNormal = new BABYLON.Vector3(planeMatrix.m[8], planeMatrix.m[9], planeMatrix.m[10]);
+                    planeNormal.normalize();
+      
+                    let angle = Math.atan2(planeNormal.x, planeNormal.z);
+                    model.rotation.y = angle + Math.PI / 2;
+      
+                    conV++;
+                }
+            }
+            console.log("====================================================");
         });
 
         this._xrPlanes.onPlaneUpdatedObservable.add((plane: IWebXRPlaneWithMesh) => {
@@ -104,23 +164,25 @@ export class XrExperienceWithPlaneDetection {
         }
     }
 
-    initPolygon(plane: IWebXRPlane, mat?: StandardMaterial): Mesh {
+    initPolygon(plane: IWebXRPlaneWithMesh, mat?: StandardMaterial): Mesh {
         plane.polygonDefinition.push(plane.polygonDefinition[0]);
         const polygonTriangulation = new PolygonMeshBuilder(plane.xrPlane.orientation, plane.polygonDefinition.map((p) => new Vector2(p.x, p.z)), this._scene);
         const polygon = polygonTriangulation.build(false, 0.01);
 
         polygon.createNormals(false);
 
+        plane.mesh = polygon;
+
         if (mat) {
             polygon.material = mat;
         }
 
-        polygon.rotationQuaternion = new Quaternion();
-        polygon.checkCollisions = true;
-        polygon.receiveShadows = true;
+        plane.mesh.rotationQuaternion = new Quaternion();
+        plane.mesh.checkCollisions = true;
+        plane.mesh.receiveShadows = true;
 
-        plane.transformationMatrix.decompose(polygon.scaling, polygon.rotationQuaternion, polygon.position);
-
+        //plane.transformationMatrix.decompose(polygon.scaling, polygon.rotationQuaternion, polygon.position);
+        plane.transformationMatrix.decompose(plane.mesh.scaling, plane.mesh.rotationQuaternion, plane.mesh.position);
         this._planes[plane.id] = (polygon);
 
         return polygon;
